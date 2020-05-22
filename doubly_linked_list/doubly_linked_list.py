7d47b84345850713b0faba8ac2de0a0ff0f3499e
kly001 <karenly1961@gmail.com>
2020-05-21 22:02:35 -0400

    Finished doubly_linked_list.py

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -44,6 +44,7 @@ class DoublyLinkedList:
     def __len__(self):
         return self.length
 
+
     """Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
@@ -95,15 +96,18 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
+
+        value = node.value
         self.delete(node)
-        self.add_to_head(node.value)
+        self.add_to_head(value)
 
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
+        value = node.value
         self.delete(node)
-        self.add_to_tail(node.value)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
@@ -137,5 +141,21 @@ class DoublyLinkedList:
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        value = self.head.value 
+        current = self.head
+        while current is not None:
+            if current.value > value:
+                value = current.value 
+            current = current.next
+        return value 
     
+        # highest_value = self.head.value
+        # current_node = current_node.next
+
+        # while current_node is not None:
+        #     current_node = current_node.next
+        #     if current_node.value:
+        #         highest_value = current_node.value
+        #     current_node = current_node.next
+
+        # return highest_value
