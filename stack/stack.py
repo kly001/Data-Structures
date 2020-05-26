71cde209a2560a22786dbb28da6cbaddcc08d0d3
kly001 <karenly1961@gmail.com>
2020-05-26 00:23:24 -0400

    Copied DLL into files as needed

--- stack/stack.py
+++ stack/stack.py
@@ -26,13 +26,159 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-import sys
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
 
-sys.path.append('..\doubly_linked_list.py')
+    """Wrap the given value in a ListNode and insert it
+    after this node. Note that this node could already
+    have a next node it is point to."""
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
 
-from doubly_linked_list import DoublyLinkedList
+    """Wrap the given value in a ListNode and insert it
+    before this node. Note that this node could already
+    have a previous node it is point to."""
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, current_prev, self)
+        if current_prev:
+            current_prev.next = self.prev
+
+    """Rearranges this ListNode's previous and next pointers
+    accordingly, effectively deleting this ListNode."""
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
 
 
+"""Our doubly-linked list class. It holds references to
+the list's head and tail nodes."""
+class DoublyLinkedList:
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+
+    def __len__(self):
+        return self.length
+
+    def find_middle(self):
+        middle = self.head
+        end = self.head
+        while end.next != None and end.next.next != None:
+            end = end.next.next
+            middle = middle.next
+        return middle
+
+
+    # def iterate_list(node):
+    #     while node is not None:
+    #         print(node.value)
+    #         node = node.next
+
+
+    """Wraps the given value in a ListNode and inserts it 
+    as the new head of the list. Don't forget to handle 
+    the old head node's previous pointer accordingly."""
+    def add_to_head(self, value):
+        # wrap the given value in a ListNode
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        # handle if list has a head
+        if self.head:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        # handle if list has no head
+        else:
+            self.head = new_node
+            self.tail = new_node
+
+    """Removes the List's current head node, making the
+    current head's next node the new head of the List.
+    Returns the value of the removed Node."""
+    def remove_from_head(self):
+        value = self.head.value
+        self.delete(self.head)
+
+        return value
+
+    """Wraps the given value in a ListNode and inserts it 
+    as the new tail of the list. Don't forget to handle 
+    the old tail node's next pointer accordingly."""
+    def add_to_tail(self, value):
+        new_node = ListNode(value, None, None)
+        self.length += 1
+
+        if self.tail:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+        else:
+            self.head = new_node
+        self.tail = new_node
+
+    """Removes the List's current tail node, making the 
+    current tail's previous node the new tail of the List.
+    Returns the value of the removed Node."""
+    def remove_from_tail(self):
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new head node of the List."""
+    def move_to_front(self, node):
+
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
+
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new tail node of the List."""
+    def move_to_end(self, node):
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
+
+    """Removes a node from the list and handles cases where
+    the node was the head or the tail"""
+    def delete(self, node):
+
+        # if list is empty
+        if not self.head:
+            print("you got nothing on me!")
+            return
+        
+        self.length -= 1
+
+        # if list has just one item
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+
+     # we have at least two nodes, and the node we want to delete is the head
+        if node == self.head:
+            self.head = node.next
+            self.head.prev = None
+        
+        # we have at least two nodes, and the node we want to delete is the tail
+        if node == self.tail:
+            self.tail = node.prev
+            self.tail.next = None
+
+        else:
+            node.delete()
+
+#################################################################
 
 class Stack:
     def __init__(self):
