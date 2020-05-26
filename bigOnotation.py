6ccd2a50bbe0877842d9781c61f656a2742e7238
kly001 <karenly1961@gmail.com>
2020-05-26 13:37:51 -0400

    Adding bigOnotation.py from class

---
+++ bigOnotation.py
@@ -0,0 +1,26 @@
+def find_name(name, phone_book):
+    a = 2
+    b = 10**ac = 3+4
+    for person in phone_book:
+        if name == person:
+            print('hello')
+
+    for person in phone_book:
+        if name == person:
+            return True
+
+name = "Kim"
+phone_book = ["Oliver", "BobbyG", "Tim", "Dan", "Oliver", "Kim"]
+
+#  O(n + n) --> O(n)
+
+def find_name(austin_phone_book, philly_phone_book):
+    for philadelphian in philly_phone_book:
+        if austinite == philadelphian:
+            return True
+
+austin_phone_book = ["Oliver", "BobbyG", "Tim", "Dan", "Oliver", "Kim"]
+philly_phone_book = ["Patrick", "Brandy", "Soosh", "Jen", "Carl", "Ace"]
+
+O(n^2)
+O(16)
